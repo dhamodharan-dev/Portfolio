284b1c17f4e55ef4b04142009e3ca4441c6fcf85
dhomdharan-dev <dhamu1908@gmail.com>
2024-05-19 11:05:31 +0530

    updated

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -9,7 +9,7 @@ function Contact() {
             <h1 className='title' id='contact'>Social handles</h1>
             <div className='social-icons'>
                 <a target='_blank' href='https://www.instagram.com/imdhamu/' className='contact-icon' id='insta'><FontAwesomeIcon icon={faInstagram} title="Instagram" /></a>
-                <a target='_blank' href='https://www.linkedin.com/in/dhamodharan-b-aa315a243/' className='contact-icon' id='lin'><FontAwesomeIcon icon={faLinkedin} title="LinkedIn" /></a>
+                <a target='_blank' href='https://www.linkedin.com/in/dhamodharan-dev' className='contact-icon' id='lin'><FontAwesomeIcon icon={faLinkedin} title="LinkedIn" /></a>
                 <a target='_blank' href='https://github.com/Dhamodharan-dev' className='contact-icon'><FontAwesomeIcon icon={faGithub} title="GitHub" /></a>
             </div>
             <p><a href="mailto:myself.dhamodharan@gmail.com" className='email'>Email - myself.dhamodharan@gmail.com</a></p>
